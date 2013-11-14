ac0af6bc4a456bcf63bdc410bfefe59a4a8a6c0c
kph1g10 <kph1g10@ecs.soton.ac.uk>
2013-11-14 14:43:33 +0000

    Removed 2013 from footer
    Removed 2013 from footer as the current year is pulled through already.

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 			<div id="footer" class="row">
-				<p class="col-md-offset-1 text-muted">Designed by Boyd's Artistic Design Company <?=date('Y');?> 2013</p>
+				<p class="col-md-offset-1 text-muted">Designed by Boyd's Artistic Design Company <?=date('Y');?></p>
 			</div>
 			
 		</div><!-- /container -->
